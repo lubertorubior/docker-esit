a5c3226323c9f90997ef7b3c8325817d6592abf1
Luis A. Rubio <lubertorubior@gmail.com>
2018-03-16 10:32:47 +0000

    Fixed download from github

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -7,8 +7,12 @@ TESTS_URL="https://github.com/lubertorubior/docker-esit/raw/master/test/test.tar
 tests_path=${1:-/tmp/tests}
 
 # Download and unzip tests
-cd ${tests_path}; curl -O -J ${TESTS_URL}; unzip test.zip; cd -
+mkdir -p ${tests_path}
+cd ${tests_path}; curl -LJO ${TESTS_URL}; tar -xvzf test.tar.gz; cd -
 
 # Run tests
-find ${tests_path} -name "run.sh" -type f -execdir { chmod +x run.sh; ./run.sh } \;
+find ${tests_path} -name "run.sh" -type f -execdir ./run.sh \;
+
+# Clean up directories
+rm -rf ${tests_path}
 
