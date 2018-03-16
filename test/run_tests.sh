0a224102c288ab5a758de695fa3e428debe34b9c
Luis A. Rubio <lubertorubior@gmail.com>
2018-03-16 09:49:19 +0000

    Compress tests

---
+++ test/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Tests URL
+TESTS_URL=""
+
+# Input file path
+tests_path=${1:-/tmp/tests}
+
+# Download tests
+cd ${tests_path}; curl -O -J ${TESTS_URL}; cd -
+
+# Run tests
+find ${tests_path} -name "run.sh" -type f -execdir { chmod +x run.sh; ./run.sh } \;
+
