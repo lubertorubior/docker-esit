8b53fc1fff16c8991f25474c8b1b2f980b129243
Luis A. Rubio <lubertorubior@gmail.com>
2018-03-16 09:52:57 +0000

    Add tests URL

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Tests URL
-TESTS_URL=""
+TESTS_URL="https://github.com/lubertorubior/docker-esit/raw/master/test/test.zip"
 
 # Input file path
 tests_path=${1:-/tmp/tests}
 
-# Download tests
-cd ${tests_path}; curl -O -J ${TESTS_URL}; cd -
+# Download and unzip tests
+cd ${tests_path}; curl -O -J ${TESTS_URL}; unzip test.zip; cd -
 
 # Run tests
 find ${tests_path} -name "run.sh" -type f -execdir { chmod +x run.sh; ./run.sh } \;
