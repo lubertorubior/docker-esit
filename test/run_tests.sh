3dd098c68e7c6e386a24a76a3bfc9b9fbc3b8ce1
Luis A. Rubio <lubertorubior@gmail.com>
2018-03-19 09:21:03 +0000

    Download tests using git

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Tests URL
-TESTS_URL="https://github.com/lubertorubior/docker-esit/raw/master/test/test.tar.gz"
+REPO_URL="https://github.com/lubertorubior/docker-esit"
+TEST_DIR="test"
 
 # Input file path
-tests_path=${1:-/tmp/tests}
+tests_path="${1:-/tmp/docker-tests}"
 
-# Download and unzip tests
-mkdir -p ${tests_path}
-cd ${tests_path}; curl -LJO ${TESTS_URL}; tar -xvzf test.tar.gz; cd -
+# Download tests from repo
+echo "Downloading tests from repository..."
+mkdir -p "${tests_path}"
+cd "${tests_path}"
+git init
+git remote add origin -f "${REPO_URL}"
+git config core.sparsecheckout true
+echo "${TEST_DIR}" > .git/info/sparse-checkout
+git pull --depth=1 origin master
+cd - &>/dev/null
+echo "Download complete"
 
 # Run tests
-find ${tests_path} -name "run.sh" -type f -execdir ./run.sh \;
+find "${tests_path}" -name "run.sh" -type f -execdir ./run.sh \;
 
 # Clean up directories
-rm -rf ${tests_path}
-
+rm -rf "${tests_path}"
