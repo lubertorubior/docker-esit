322b349a107346c9fbf2911023ad65610cbdf82a
Pedro Gonzalez <pgonyan@gmail.com>
2018-03-22 14:59:40 +0000

    web and others

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in hadoop hadoop-namenode hadoop-datanode hadoop-nodemanager hadoop-resourcemanager hadoop-historyserver spark spark-historyserver bio; do
+for i in hadoop hadoop-namenode hadoop-datanode hadoop-nodemanager hadoop-resourcemanager hadoop-historyserver spark spark-historyserver bio web; do
   echo Building $i
   (cd $i && ./build.sh)
 done
